87321d46d65a84669111eee5503c313082f2b963
ArchieMeng <archiemeng@protonmail.com>
2022-03-09 18:45:16 +0800

    Update realsr-ncnn-vulkan backend to be compatible with the latest version
    Signed-off-by: ArchieMeng <archiemeng@protonmail.com>

--- utils/processors/realsr_ncnn_vulkan/processor.py
+++ utils/processors/realsr_ncnn_vulkan/processor.py
@@ -1,20 +1,36 @@
+from math import floor
+
 from PIL import Image
 
-from .realsr_ncnn_vulkan import RealSR
+try:
+    # import from local directory
+    from .realsr_ncnn_vulkan import RealSR
+except ImportError:
+    # import from installed package
+    from realsr_ncnn_vulkan_python import RealSR
 from ..params import ProcessParams
 from ..simple import BaseProcessor
 
 
 class Processor(BaseProcessor):
     def __init__(self, params: ProcessParams):
+        self.params = params
         self.upscaler = RealSR(
             params.device_id,
             params.model or "models-DF2K",
             params.tta_mode,
-            params.scale,
-            params.tilesize,
-            params.debug
+            4,
+            params.tilesize
         )
 
     def process(self, im: Image) -> Image:
-        return self.upscaler.process(im)
+        if self.params.scale > 1:
+            cur_scale = 1
+            w, h = im.size
+            while cur_scale < self.params.scale:
+                im = self.upscaler.process(im)
+                cur_scale *= 4
+            w, h = floor(w * self.params.scale), floor(h * self.params.scale)
+            im = im.resize((w, h))
+
+        return im
